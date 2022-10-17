34d7a92844a3adedddcde606fadd40f171e4fe8d
Achette <i.achette@live.com>
2022-10-17 14:01:26 -0300

    Tests: added testes for logo component

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -8,7 +8,7 @@ describe('<Logo />', () => {
   it('should render a white label by default', () => {
     // renderizar o componente --> render
     // selecionar o elemento a ser testado --> screen (queries) --> getBy...
-    // expect --> asseertion (asserção - compararão - análise(espero que renderize...))
+    // expect --> assertion (asserção - compararão - análise(espero que renderize...))
 
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
@@ -20,6 +20,20 @@ describe('<Logo />', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a normal logo when size is not passed', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
     })
   })
 })
